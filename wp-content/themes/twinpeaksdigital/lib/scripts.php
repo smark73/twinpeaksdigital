251a2fb1665e8d13d4f4d45797135e4bf3ff8b9c
Stacy at home <stacy@stacymark.com>
2014-07-09 15:11:24 -0700

    remove negative margin on tagline so it shows better on mobile

--- wp-content/themes/twinpeaksdigital/lib/scripts.php
+++ wp-content/themes/twinpeaksdigital/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '66a346167a7d0b9a928e8bc44519d26c');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ae635d2399640280993102e19a2f7ed3');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
