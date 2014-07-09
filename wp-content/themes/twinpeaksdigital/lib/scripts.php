f017a9254ac286df3898c09418890000fbafac04
Stacy at home <stacy@stacymark.com>
2014-07-09 15:09:27 -0700

    squeeze tagline to 95% so it shows better on mobile

--- wp-content/themes/twinpeaksdigital/lib/scripts.php
+++ wp-content/themes/twinpeaksdigital/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1cf230feb9c892515952523773366480');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '66a346167a7d0b9a928e8bc44519d26c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
