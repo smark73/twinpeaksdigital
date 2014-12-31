50cf0f0874eb3f83eaad710ac5a8616357090dff
Stacy at home <stacy@stacymark.com>
2014-12-31 14:15:13 -0800

    updated Portfolio page header text

--- wp-content/themes/twinpeaksdigital/lib/scripts.php
+++ wp-content/themes/twinpeaksdigital/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'fafe8117a70a28c50a91a16f302b6a6b');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1c82d49aff45ceb5203329fe44d0660f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
