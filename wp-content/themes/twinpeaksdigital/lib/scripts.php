bc31df8fdadeac44e0eb63a0ac7a69ee824451e1
Stacy at home <stacy@stacymark.com>
2015-05-12 13:44:43 -0700

    dk navbar with red blue ochre

--- wp-content/themes/twinpeaksdigital/lib/scripts.php
+++ wp-content/themes/twinpeaksdigital/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '96e8bbd0f543d070399c2e07b43b3161');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f482d7b24bcd98b6f0a6ec11072c9522');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
