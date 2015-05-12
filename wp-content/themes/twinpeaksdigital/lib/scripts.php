2e988d4c435d980996b608f66609199b943aa8cf
Stacy at home <stacy@stacymark.com>
2015-05-12 13:13:33 -0700

    updated colors

--- wp-content/themes/twinpeaksdigital/lib/scripts.php
+++ wp-content/themes/twinpeaksdigital/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ee16035e6a9150e91d04515e1d21fd4b');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '97e400ff168d5b3e37db7a57fc85fc0a');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
